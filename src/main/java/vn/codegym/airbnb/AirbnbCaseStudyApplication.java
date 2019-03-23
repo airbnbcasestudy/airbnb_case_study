198bac6c45deda98781513df06190935807094aa
Quyen Le Sy <danquyen6596@gmail.com>
2019-03-23 21:17:37 +0700

    Add all file config to program.

---
+++ src/main/java/vn/codegym/airbnb/AirbnbCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package vn.codegym.airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbCaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/vn/codegym/airbnb/controller/LoginController.java
@@ -0,0 +1,56 @@
+package vn.codegym.airbnb.controller;
+
+
+import org.apache.http.client.ClientProtocolException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vn.codegym.airbnb.common.RestFb;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Controller
+public class LoginController {
+    private static final String BASE_DIR = "/login";
+
+    @Autowired
+    private RestFb restFb;
+    @RequestMapping(value = { "/", "/login" })
+    public String login() {
+        return BASE_DIR + "/login";
+    }
+    @RequestMapping("/login-facebook")
+    public String loginFacebook(HttpServletRequest request) throws ClientProtocolException, IOException {
+        String code = request.getParameter("code");
+
+        if (code == null || code.isEmpty()) {
+            return "redirect:/login?facebook=error";
+        }
+        String accessToken = restFb.getToken(code);
+
+        com.restfb.types.User user = restFb.getUserInfo(accessToken);
+        UserDetails userDetail = restFb.buildUser(user);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetail, null,
+                userDetail.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "redirect:/user";
+    }
+    @RequestMapping("/user")
+    public String user() {
+        return BASE_DIR + "/user";
+    }
+    @RequestMapping("/admin")
+    public String admin() {
+        return BASE_DIR + "/admin";
+    }
+    @RequestMapping("/403")
+    public String accessDenied() {
+        return "/error/403";
+    }
+}
